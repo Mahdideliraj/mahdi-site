58a73e46ba94053501f9ac24594abac68417cab2
mahdi <mahdide7@gmail.com>
2023-09-30 12:50:10 +0330

    first wordpress project

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+
+function mahdi_file(){
+    wp_enqueue_script('js-file' ,get_theme_file_uri('/build/index.js'), array('jquery'),'1.0',true);
+    wp_enqueue_style('custome-google-font' ,'//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700is');
+    wp_enqueue_style('font-awesome' ,'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('mahdi_main_style',get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('mahdi_extra_style',get_theme_file_uri('/build/index.css'));
+
+}
+
+add_action('wp_enqueue_scripts','mahdi_file');
+
+
+function mahdi_features(){
+    
+
+    add_theme_support("title-tag");
+}
+
+
+function mahdi_adjust_query($query) {
+    if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
+        $today = date('Ymd');
+    $query->set('meta_key','event_date');
+    $query->set('orderby','meta_value_num');
+    $query->set('order','ASC');
+    $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today ,
+          'type' => 'numeric'
+        )
+      ));
+
+}
+}
+
+add_action('pre_get_posts','mahdi_adjust_query');
+?>
