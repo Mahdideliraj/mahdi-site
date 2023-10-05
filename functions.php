754bbfa0f222515bb3486ccfd41f1221180da9b1
mahdi <mahdide7@gmail.com>
2023-10-05 18:38:26 +0330

    add size

--- functions.php
+++ functions.php
@@ -17,6 +17,8 @@ function mahdi_features(){
 
     add_theme_support("title-tag");
     add_theme_support("post-thumbnails");
+    add_image_size('professorLandscape',400,260,true);
+    add_image_size('professorPortrait',480,650,true);
 }
 
 
